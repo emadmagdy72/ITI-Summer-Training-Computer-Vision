90d0d89a37e925c65da0038aaa877ca88549de89
emadmagdy72 <emadmagdy7272@gmail.com>
2023-05-28 01:39:32 +0300

    Tasks of day 4

--- Day 01/Q1.py
+++ Day 01/Q1.py
@@ -1,33 +1,8 @@
-# lst=[20,30,45,10,60]
-# lst.sort()
-# print("The second highest number is:",lst[-2])
+lst=[20,30,45,10,60]
+lst.sort()
+print("The second highest number is:",lst[-2])
 
 
-
-
-def CodelandUsernameValidation(strParam):
-
-  if len(strParam) < 4 or len(strParam) > 25:
-    return False
-
-  if not strParam[0].isalpha():
-    return False
-  
-  if not (strParam.isalnum() and '_' in strParam):
-    print(strParam.isalnum())
-    print('_' in strParam)
-    return False
-
-  if strParam[-1] == '_':
-    return False
-
-
-
-  # code goes here
-  return True
-
-# keep this function call here 
-print(CodelandUsernameValidation(input()))
 
 
 
